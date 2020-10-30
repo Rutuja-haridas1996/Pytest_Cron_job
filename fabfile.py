70176fdea3294ea24d314c9fa1755b920fbe7b9f
Rutuja Haridas <rutuja.haridas@launchventures.co>
2020-10-30 17:47:07 +0530

    added params

--- fabfile.py
+++ fabfile.py
@@ -1,9 +1,8 @@
 from __future__ import with_statement
-from fabric.api import local, settings, abort, run, get
+from fabric.api import *
 from fabric.contrib.console import confirm
 
-
-
+env.PASSWORD = "root@123"
 
 def pwd():
     run("pwd")
