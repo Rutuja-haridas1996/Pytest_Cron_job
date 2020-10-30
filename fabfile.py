b913441519f70d54daee704c3413609f9095165d
Rutuja Haridas <rutuja.haridas@launchventures.co>
2020-10-30 20:40:33 +0530

    add params

--- fabfile.py
+++ fabfile.py
@@ -2,17 +2,9 @@ from __future__ import with_statement
 from fabric.api import *
 from fabric.contrib.console import confirm
 
-# env.hosts = [
-#     'rutujaharidas-HP-Pavilion-Laptop-15-cc1xx'
-# ]
-#
-# env.password = "root@123"
 
 def pwd():
-    sudo("pwd")
-
-
-
+    run("pwd")
 
 def run_backup_script():
     #result = run("chmod +x /home/rutujaharidas/PycharmProjects/jenkins_3/Pytest_Cron_job/db_backup_script.sh")
