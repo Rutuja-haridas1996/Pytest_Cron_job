47d2fec48cb25273734d85f997069ab011ae3bb2
Rutuja Haridas <rutuja.haridas@launchventures.co>
2020-11-02 11:34:29 +0530

    params

--- fabfile.py
+++ fabfile.py
@@ -7,7 +7,6 @@ def hello(ctx):
 
 @task
 def connect(ctx,user,hostname,password):
-  #conn=Connection(host="rutujaharidas-HP-Pavilion-Laptop-15-cc1xx", connect_kwargs={"password": "root@123"})
   conn=Connection(host = hostname,
   user = user,
   connect_kwargs = {
